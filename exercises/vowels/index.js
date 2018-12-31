301b925f1cda39a61bdfdf0c2fefebf6219a3ac9
Dakaney <wayman_ng@yahoo.com>
2018-12-30 19:50:50 -0800

    completed vowels

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,13 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+    let storage = {'a': true, 'e': true, 'i': true, 'o': true, 'u': true};
+    let count = 0;
+    for (let char of str.toLowerCase().split('')) {
+        if (storage[char]) count += 1;
+    }
+    return count;
+}
 
 module.exports = vowels;
