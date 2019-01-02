404be5cd328d18e5b8de5f789fb3ef70f7270ada
Dakaney <wayman_ng@yahoo.com>
2019-01-01 19:36:37 -0800

    matrix

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -16,21 +16,6 @@
 //     [10,  9,  8, 7]]
 
 function matrix(n) {
-    let matrix = [];
-    for (let i = 0; i < n; i++) {
-        matrix.push([]);
-    }
-    let row = 0;
-    let col = 0;
-    let incR = 0;
-    let incC = 1;
-    for (let i = 1; i <= n * n; i++) {
-        matrix[row][col] = i;
-        if ()
-        row = row + incR;
-        col = col + incC;
-
-    }
 }
 
 module.exports = matrix;
