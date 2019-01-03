950dd07312289ecc860937c64537de67ee1d5477
Dakaney <wayman_ng@yahoo.com>
2019-01-03 09:05:43 -0800

    completed queue

--- exercises/queue/index.js
+++ exercises/queue/index.js
@@ -8,6 +8,18 @@
 //     q.add(1);
 //     q.remove(); // returns 1;
 
-class Queue {}
+class Queue {
+    constructor () {
+        this.storage = [];
+        this.position = 0;
+    }
+    add (value) {
+        this.storage.unshift(value);
+    }
+    remove () {
+        let removed = this.storage.pop();
+        return removed;
+    }
+}
 
 module.exports = Queue;
